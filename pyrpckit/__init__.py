ad06444024e4c7a5176d2112be312f69c19b32f2
mathisarends <mathisarends27@gmail.com>
2026-09-10 21:44:25 +0200

    Prepare release 0.4.0

--- pyrpckit/__init__.py
+++ pyrpckit/__init__.py
@@ -18,7 +18,7 @@ from .models import RpcModel
 from .router import RpcModule
 from .server import RpcErrorMapper, RpcServer
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 __all__ = [
     "ProtocolDefinitionError",
